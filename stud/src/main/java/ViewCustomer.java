d07d0c39edf03aee84928ea4de04249484687428
Niklas Meyer <NiklasM@vpnex-069.uni-duisburg.de>
2016-11-18 13:49:03 +0100

    Beauty fixes

--- stud/src/main/java/ViewCustomer.java
+++ stud/src/main/java/ViewCustomer.java
@@ -5,11 +5,9 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -21,10 +19,11 @@ import javafx.stage.Stage;
 import javax.swing.*;
 
 public class ViewCustomer extends BorderPane{
-
+    private HBox order= new HBox();
     private VBox v1 = new VBox();
     private VBox v2 = new VBox();
     private HBox h1 = new HBox();
+    private Label l1 = new Label("Your Orders");
     private Button buyButton = new Button("Buy");
     private TableView tv = new TableView();
     private ListView lv = new ListView();
@@ -55,11 +54,19 @@ public class ViewCustomer extends BorderPane{
         v1.getChildren().add(lv);
         v2.getChildren().add(tv);
         h1.getChildren().add(buyButton);
+        order.getChildren().add(l1);
+        h1.setMargin(buyButton, new Insets(20,0,20,0));
+        order.setStyle("-fx-background-color: #55AA00, linear-gradient(#55AA00 50%, white 100%), radial-gradient(center 50% -40%, radius 200%, #e6e6e6 45%, rgba(230,230,230,0) 50%);");
+        order.setMargin(l1, new Insets(20,50,20,50));
+        order.setAlignment(Pos.CENTER);
+        l1.setStyle("-fx-font-family: Avenir Ultra Light; -fx-font-size: 28; ");
 
         this.setRight(v2);
         this.setLeft(v1);
         this.setBottom(h1);
-        h1.setAlignment(Pos.CENTER_RIGHT);
+        this.setTop(order);
+        h1.setAlignment(Pos.CENTER);
         buyButton.setMinSize(200,30);
+        buyButton.setStyle("-fx-background-color: #c3c4c4, linear-gradient(#d6d6d6 50%, white 100%), radial-gradient(center 50% -40%, radius 200%, #e6e6e6 45%, rgba(230,230,230,0) 50%); -fx-background-radius: 30; -fx-background-insets: 0,1,1;  -fx-text-fill: black; -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 3, 0.0 , 0 , 1 );");
     }
 }
