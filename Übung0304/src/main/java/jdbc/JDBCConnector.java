0b3c2e9e66a26f7ac5a871952a0f30513b741a13
Henry van der Vegte <jan-henry.van-der-vegte@uni-due.de>
2016-12-09 13:51:06 +0100

    Aufgabe 2 (X)

--- Übung0304/src/main/java/jdbc/JDBCConnector.java
+++ Übung0304/src/main/java/jdbc/JDBCConnector.java
@@ -1,5 +1,7 @@
 package jdbc;
 
+import model.Product;
+
 import java.sql.*;
 
 /**
@@ -8,23 +10,32 @@ import java.sql.*;
 public class JDBCConnector {
 
 
+    private String url =  "jdbc:postgresql://java.is.uni-due.de/ws1011";
+    private String user =  "ws1011";
+    private String password = "ftpw10";
+
+
     public  static  void main(String args[]){
 
         JDBCConnector jdbcConnector = new JDBCConnector();
-        jdbcConnector.execute();
+        //jdbcConnector.printInfos();
+       // System.out.println(jdbcConnector.insert("wudrst",10,2));
+        Product p = jdbcConnector.read(11453);
+        if(p != null){
+            System.out.println(p.getId());
+            System.out.println(p.getName());
+            System.out.println(p.getPrice());
+            System.out.println(p.getQuantity());
+        }
     }
 
-
-    private void execute(){
-        try (Connection con =
-                     DriverManager.getConnection( "jdbc:postgresql://java.is.uni-due.de/ws1011" , "ws1011" , "ftpw10"))
+    private void printInfos(){
+        try (Connection con = DriverManager.getConnection(url, user, password))
         {
-           // doSomething(con);
             DatabaseMetaData databaseMetaData = con.getMetaData();
 
-
-             System.out.println(databaseMetaData.getURL());
-             System.out.println(databaseMetaData.getUserName());
+            System.out.println("URL : " + databaseMetaData.getURL());
+            System.out.println("Username: " + databaseMetaData.getUserName());
 
             String   catalog          = null;
             String   schemaPattern    = null;
@@ -34,6 +45,7 @@ public class JDBCConnector {
             ResultSet result = databaseMetaData.getTables(
                     catalog, schemaPattern, tableNamePattern, types );
 
+            System.out.println("Tables:");
             while(result.next()) {
                 String tableName = result.getString(3);
                 System.out.println(tableName);
@@ -43,21 +55,59 @@ public class JDBCConnector {
         { e.printStackTrace() ; }
     }
 
-    private void doSomething(Connection con) throws SQLDataException {
-        try (Statement stmt = con.createStatement() ;
-             ResultSet rs = stmt.executeQuery("SELECT id , name, wohnort FROM lieferanten")) {
+    private long insert(String name, double price, int quantity){
+        try (Connection con = DriverManager.getConnection(url, user, password);
+              PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO products (name, price, quantity) VALUES(?,?,?)",
+                                                                            Statement.RETURN_GENERATED_KEYS);
+        )
+        {
+            preparedStatement.setString(1 ,name);
+            preparedStatement.setDouble(2, price);
+            preparedStatement.setInt(3,quantity);
+            preparedStatement.executeUpdate();
 
-            while (rs.next()) {
-                int idVal = rs.getInt("id");
-                String nameVal = rs.getString("name");
-                String wohnortVal = rs.getString("wohnort");
-                System.out.println("id = " + idVal + " , name = " + nameVal + " , wohnort = " + wohnortVal);
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+            if (rs != null && rs.next()) {
+                return rs.getLong(1);
             }
-        } catch (SQLException e)
-        { e.printStackTrace() ; }
+        }catch (SQLException e)
+        {
+            e.printStackTrace() ;
+        }
+
+        return  -1;
+    }
+
+    private void insert(Product product){
+        long id = insert(product.getName(), product.getPrice(), product.getQuantity());
+        product.setId(id);
+    }
+
+    private Product read(long productID){
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             PreparedStatement preparedStatement = con.prepareStatement("SELECT id, name, price, quantity FROM products WHERE id=? ");
+        )
+        {
+            preparedStatement.setLong(1, productID);
 
 
+            ResultSet rs = preparedStatement.executeQuery();
+            Product p = new Product();
 
 
+            while (rs.next()){
+                p.setId(rs.getLong(1));
+                p.setName(rs.getString(2));
+                p.setPrice(rs.getDouble(3));
+                p.setQuantity(rs.getInt(4));
+            }
+
+            return p;
+        }catch (SQLException e)
+        {
+            e.printStackTrace() ;
+        }
+
+        return  null;
     }
 }
